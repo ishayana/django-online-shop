930b81c1f926160f1f7cc42223e1f78f4433f2ed
ishayana <shayaniza@yahoo.com>
2024-05-27 16:40:57 +0330

    Implementiation login and expire verification code

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import views
 from django.views import View
 from .forms import UserRegistrationForm, UserLoginForm, UserVerifyForm
 from .models import User, OneTimePass
 import random
+from datetime import datetime, timedelta
+from django.contrib.auth import login, authenticate, logout
 from .utils import send_sms
 from django.contrib import messages
 
@@ -43,17 +46,25 @@ class UserVerifyView(View):
     def post(self, request):
         user_session = request.session['user_registration_info']
         code_instance = OneTimePass.objects.get(phone=user_session['phone'])
+        now = datetime.now()
+        expire_code = now + timedelta(minutes=2)
         form = self.form_class(request.POST)
         if form.is_valid():
-            if form.cleaned_data['code'] == code_instance.code:
-                User.objects.create_user(user_session['email'], user_session['phone'], user_session['full_name'], user_session['password'])
-                code_instance.delete()
-                messages.success(request, 'You registered succussfully!', 'success')
-                print('code success!')
-                return redirect('home:home')
+            if now < expire_code:
+                if form.cleaned_data['code'] == code_instance.code:
+                    User.objects.create_user(user_session['email'], user_session['phone'], user_session['full_name'], user_session['password'])
+                    user = authenticate(request, phone=user_session['phone'], password=user_session['password'])
+                    login(request, user)
+                    code_instance.delete()
+                    messages.success(request, 'You registered succussfully!', 'success')
+                    return redirect('home:home')
+                else:
+                    messages.error(request, 'Enterd code is wrong!', 'error')
+
+                    return redirect('accounts:verify')
             else:
-                messages.error(request, 'Enterd code is wrong!', 'error')
-                print('code wrong!')
+                messages.error(request, 'Your code is expierd!')
+                code_instance.delete()
                 return redirect('accounts:verify')
         else:
             return render(request, self.template_name, {'form' : form})
@@ -68,4 +79,14 @@ class UserLoginView(View):
         return render(request, self.template_name, {'form': form})
     
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, phone=cd['phone'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Login successfully.')
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+            messages.error(request, 'Username or password is incorect!', 'error')
+            return redirect('accounts:login')
+        return render(request, self.template_name, {'form' : form})
